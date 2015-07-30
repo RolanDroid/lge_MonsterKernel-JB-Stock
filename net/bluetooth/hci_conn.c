9d1709523e79b341070064cdfaa42bd371052e66
RolanDroid <rolandosimoncelli8@gmail.com>
2015-07-30 18:58:44 +0200

    Fixed some error by LG

--- net/bluetooth/hci_conn.c
+++ net/bluetooth/hci_conn.c
@@ -305,7 +305,7 @@ void hci_le_start_enc(struct hci_conn *conn, __le16 ediv, __u8 rand[8],
 	memset(&cp, 0, sizeof(cp));
 
 	cp.handle = cpu_to_le16(conn->handle);
-        memcpy(cp.ltk, ltk, sizeof(*ltk));
+	memcpy(cp.ltk, ltk, sizeof(cp.ltk));
 	cp.ediv = ediv;
 	memcpy(cp.rand, rand, sizeof(cp.rand));
 
@@ -323,7 +323,7 @@ void hci_le_ltk_reply(struct hci_conn *conn, u8 ltk[16])
 	memset(&cp, 0, sizeof(cp));
 
 	cp.handle = cpu_to_le16(conn->handle);
-	memcpy(cp.ltk, ltk, sizeof(ltk));
+	memcpy(cp.ltk, ltk, sizeof(*ltk));
 
 	hci_send_cmd(hdev, HCI_OP_LE_LTK_REPLY, sizeof(cp), &cp);
 }
@@ -1354,3 +1354,4 @@ int hci_set_auth_info(struct hci_dev *hdev, void __user *arg)
 
 	return copy_to_user(arg, &req, sizeof(req)) ? -EFAULT : 0;
 }
+
