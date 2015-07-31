39e15406bbcfedfbebd1549f6cbd7b2c10b875fc
RolanDroid <rolandosimoncelli8@gmail.com>
2015-07-31 12:11:49 +0200

    Fixed nfc error

--- net/nfc/core.c
+++ net/nfc/core.c
@@ -204,12 +204,6 @@ int nfc_dep_link_up(struct nfc_dev *dev, int target_index, u8 comm_mode)
 		goto error;
 	}
 
-	gb = nfc_llcp_general_bytes(dev, &gb_len);
-	if (gb_len > NFC_MAX_GT_LEN) {
-		rc = -EINVAL;
-		goto error;
-	}
-
 	rc = dev->ops->dep_link_up(dev, target_index, comm_mode, gb, gb_len);
 
 error:
